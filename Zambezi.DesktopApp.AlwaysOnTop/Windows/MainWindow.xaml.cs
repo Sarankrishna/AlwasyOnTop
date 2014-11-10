baf034af3b5cd03a06433584c6bb73fa27d35623
saran <saran.rk@gmail.com>
2014-11-10 16:09:54 -0700

    added handler for close button

--- Zambezi.DesktopApp.AlwaysOnTop/Windows/MainWindow.xaml.cs
+++ Zambezi.DesktopApp.AlwaysOnTop/Windows/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Zambezi.DesktopApp.AlwaysOnTop.Windows
         {
             InitializeComponent();
         }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
