d55f2f0ddb8698939034a13e175f81af6875b99c
saran <saran.rk@gmail.com>
2014-11-10 16:02:02 -0700

    Initial chekcin

---
+++ Zambezi.DesktopApp.AlwaysOnTop/Classes/RECT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zambezi.DesktopApp.AlwaysOnTop.Classes
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left, Top, Right, Bottom;
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left; Top = top; Right = right; Bottom = bottom;
+        }
+        public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
+        public int X { get { return Left; } set { Right -= (Left - value); Left = value; } }
+        public int Y { get { return Top; } set { Bottom -= (Top - value); Top = value; } }
+        public int Height { get { return Bottom - Top; } set { Bottom = value + Top; } }
+        public int Width { get { return Right - Left; } set { Right = value + Left; } }
+        public static implicit operator System.Drawing.Rectangle(RECT r)
+        {
+            return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
+        }
+        public static implicit operator RECT(System.Drawing.Rectangle r) { return new RECT(r); }
+    }
+}
---
+++ Zambezi.DesktopApp.AlwaysOnTop/Classes/WindowHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zambezi.DesktopApp.AlwaysOnTop.Classes
+{
+
+    public static class WindowHelper
+    {
+        /// <summary>Returns a dictionary that contains the handle and title of all the open windows.</summary>
+        /// <returns>A dictionary that contains the handle and title of all the open windows.</returns>
+
+        public static IDictionary<IntPtr, string> GetOpenWindows()
+        {
+            IntPtr lShellWindow = GetShellWindow();
+            Dictionary<IntPtr, string> lWindows = new Dictionary<IntPtr, string>();
+
+            EnumWindows(delegate(IntPtr hWnd, int lParam)
+            {
+                if (hWnd == lShellWindow) return true;
+                if (!IsWindowVisible(hWnd)) return true;
+
+                int lLength = GetWindowTextLength(hWnd);
+                if (lLength == 0) return true;
+
+                StringBuilder lBuilder = new StringBuilder(lLength);
+                GetWindowText(hWnd, lBuilder, lLength + 1);
+
+                lWindows[hWnd] = lBuilder.ToString();
+                return true;
+
+            }, 0);
+
+            return lWindows;
+        }
+
+        delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
+
+        [DllImport("USER32.DLL")]
+        static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport("USER32.DLL")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("USER32.DLL")]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("USER32.DLL")]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("USER32.DLL")]
+        static extern IntPtr GetShellWindow();
+
+        public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        public static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        public const UInt32 SWP_SHOWWINDOW = 0x0040;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+                                                int x, int y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(HandleRef hwnd, out RECT lpRect);
+    }
+}
